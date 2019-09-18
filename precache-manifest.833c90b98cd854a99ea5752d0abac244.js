326b0482f5dc1d0cff911bd262358e305276559c
Pawan Arora <pawan.arora@voicetree.co>
2019-09-19 00:31:18 +0530

    Updates

---
+++ precache-manifest.833c90b98cd854a99ea5752d0abac244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b48d41b25f44ded0ab36ccabadb0521",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "81b2b961008a22b11b38",
+    "url": "/monster-rolodex/static/css/main.89fa5d98.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monster-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "81b2b961008a22b11b38",
+    "url": "/monster-rolodex/static/js/main.76e56ed5.chunk.js"
+  },
+  {
+    "revision": "52b3135f1aa77ef4ddba",
+    "url": "/monster-rolodex/static/js/runtime~main.b7028ca9.js"
+  }
+]);
